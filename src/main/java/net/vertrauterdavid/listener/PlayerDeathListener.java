7ad966695b39dc8e348663265855d4b86efbebfd
vertrauterdavid <vertrauterdavid@gmail.com>
2024-06-04 21:29:41 +0200

    Disabled death message for spectators

--- src/main/java/net/vertrauterdavid/listener/PlayerDeathListener.java
+++ src/main/java/net/vertrauterdavid/listener/PlayerDeathListener.java
@@ -15,6 +15,11 @@ public class PlayerDeathListener implements Listener {
     public void handle(PlayerDeathEvent event) {
         Player player = event.getEntity();
 
+        if (player.getGameMode() == GameMode.SPECTATOR) {
+            event.setDeathMessage("");
+            return;
+        }
+
         if (EventCore.getInstance().getConfig().getBoolean("Messages.PlayerDeath.Enabled")) {
             if (player.getKiller() != null) {
                 event.setDeathMessage(MessageUtil.get("Messages.PlayerDeath.Message1").replaceAll("%player%", player.getName()).replaceAll("%killer%", player.getKiller().getName()));
